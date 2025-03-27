19d065d5940f14326135fdf1d61504f636706828
mootzen <info@mootzen.de>
2025-03-27 13:42:24 +0000

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -4,6 +4,53 @@ from lxml import html
 
 url = "https://www.werder.de/tickets/heimspiele/"
 
+print(r"""\                                                                                                                          
+                                                                                                                                                                                                                                                                                                                                                                                                       
+                                                                      WW                                                                    
+                                                                        --                                                                  
+                                                                    WW  WW                                                                  
+                                                                  WW  WW  WW                                                                
+                                                                    WWWWWW  ..                                                              
+                                                                WW  WWWWWW  WW                                                              
+                                                              WW  WWWWWWWWWW  WW                                                            
+                                                                WWWWWWWWWWWWWW                                                              
+                                                            WW  WWWWWWWWWWWWWW  WW                                                          
+                                                          WW  WWWW            WW  WW                                                        
+                                                            WWWWWW              WW                                                          
+                                                        WW  WWWWWW              WW  WW                                                      
+                                                      WW  WWWW--WW                WW  WW                                                    
+                                                        WW        WWWWWWWW        WWWW                                                      
+                                                    WW  WW        WWWWWWWWWW        WW  WW                                                  
+                                                  WW  WWWW        WWWWWWWWWWWW      WWWW  WW                                                
+                                                    WWWW      WWWWWWWWWWWWWWWW      WWWWWW                                                  
+                                                WW  WWWW      WWWWWWWWWWWWWWWWWW    WWWWWW  WW                                              
+                                              WW  WWWWWW      WWWWWWWWWWWWWWWWWW    WWWWWWWW  WW                                            
+                                                WWWWWWWW::    WWWWWWWW  WWWWWWWW    WWWWWWWWWW                                              
+                                            WW  WWWWWWWWWW    WWWWWWWW  WWWWWWWW    WWWWWWWWWW  WW                                          
+                                              WW  WWWWWWWW    WWWWWWWW  WWWWWWWW    WWWWWWWW  WW                                            
+                                                  WWWWWWWW    WWWWWW      WWWWWW    WWWWWWWW                                                
+                                                WW  WWWWWW    WWWWWW      WWWWWW    WWWWWW  WW                                              
+                                                  WW  WWWWWW  WWWW          WWWW  WWWWWW  WW                                                
+                                                      WWWWWW    WW          WW    WWWWWW                                                    
+                                                    WW  WWWW                      WWWW  WW                                                  
+                                                      WW  WWWW        WW        WWWW  WW                                                    
+                                                          WWWW        WW        WWWW  --                                                    
+                                                        WW  WW      WWWWWW      WW  WW                                                      
+                                                          WW  WW    WWWWWW    WW  WW                                                        
+                                                              WW  WWWWWWWWWW  WW  ++                                                        
+                                                            WW  WWWWWWWWWWWWWW  WW                                                          
+                                                              WW  WWWWWWWWWW  WW                                                            
+                                                                  WWWWWWWWWW  WW                                                            
+                                                                WW  WWWWWW  WW                                                              
+                                                                  WW  WW  ++                                                                
+                                                                  ..  WW  WW                                                                
+                                                                    WW  WW                                                                  
+                                                                      WW                                                                    
+                                                                      ::                                                                    
+                                                            LEBENSLANG GRÜN-WEISS                                                                                                                                                                                                                                                                                                                                                                                                              
+    """)
+
+
 # Send an HTTP request to fetch the page content
 headers = {"User-Agent": "Mozilla/5.0"}  # Set user-agent to avoid blocking
 response = requests.get(url, headers=headers)
@@ -17,7 +64,7 @@ if response.status_code == 200:
     table_element = tree.xpath('/html/body/section[5]/div[1]/div/div[1]/div[3]')
 
     if table_element:
-        print("Table found using XPath!")
+        print("Tabelle Bestellfristen über XPath gefunden!")
 
         # Extract rows from the table
         rows = table_element[0].xpath(".//tr")
